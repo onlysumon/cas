11eefaaf7954c9b80812561fd1f134327248674e
Sharif <onlysumon@yahoo.com>
2014-03-15 19:49:43 -0500

    All Entity Class Added

---
+++ CAS/CAS/Models/Applicant.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CAS.Models
+{
+    public class Applicant
+    {
+        public int ApplicantId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Middle name")]
+        [MaxLength(20)]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "I am ")]
+        public char Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Marrital status")]
+        [MaxLength(10)]
+        public string MarritalStatus { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date of birth")]
+        public System.DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "Country of citizen")]
+        [MaxLength(10)]
+        public string CountryOfCitizen { get; set; }
+
+        [Required]
+        [Display(Name = "Country of birth")]
+        [MaxLength(2)]
+        public string CountryOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "Current city (City, Country)")]
+        [MaxLength(2)]
+        public string CurrentCity { get; set; }
+
+        [Required]
+        [Display(Name = "Visa status")]
+        [MaxLength(5)]
+        public string VisaStatus { get; set; }
+
+        [MaxLength(50)]
+        public string Skype { get; set; }
+
+        [Display(Name = "Phone Home")]
+        [MaxLength(50)]
+        public string PhoneHome { get; set; }
+
+        [Display(Name = "Phone Work")]
+        [MaxLength(50)]
+        public string PhoneWork { get; set; }
+
+        //Relations
+        public Application Application { get; set; }
+        public List<Education> Educations { get; set; }
+        public List<Employment> Employments { get; set; }
+        public List<AwardsAndHonors> AwardsAndHonors { get; set; }
+        public EnglishProficiency EnglishProficiency { get; set; }        
+    }
+}
---
+++ CAS/CAS/Models/Application.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CAS.Models
+{
+    public class Application
+    {
+        [Key, ForeignKey("Applicant")]
+        public int Id { get; set; }        
+
+        public DateTime StartDate { get; set; }
+        public DateTime Submission { get; set; }
+
+        [MaxLength(20)]
+        public string EvaluationStatus { get; set; }
+
+        public bool IsSubmitted { get; set; }
+        
+        public Applicant Applicant { get; set; }
+    }
+}
